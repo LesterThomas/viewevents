edd2ec3d6275f626e5c1097a1ac12be0e61f2b7b
Lester <lesterthomas@hotmail.com>
2014-12-24 20:06:13 +0000

    Added query to get Event data from database
    At presents, events are shown as a list

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,22 @@
 (function ($) {
   $(document).ready(function () {
     // you awesome code goes here
+
+    $.getJSON( '/db/_all_docs?include_docs=true' , function( data ) {
+			  //alert(JSON.stringify(data));
+			  var jsonEvents=data;
+			  var eventsArray=jsonEvents.rows;
+			  var eventHTML='<ul>';
+			  for(var i=0;i<eventsArray.length;i++){
+			  	var eventId=eventsArray[i].id;
+			  	var eventDate=eventId.split(':').pop();
+			  	var eventDataObj=new Date(parseInt(eventDate));
+			  	var theDate=eventDataObj.toString();
+			  	eventHTML=eventHTML + '<li> id:' + eventsArray[i].id + ', date:' + theDate + ', name:' + eventsArray[i].doc.Name + ', message:' + eventsArray[i].doc.Message + '</li>';
+			  }
+			  eventHTML=eventHTML + '</ul>';
+			  $('#Events').html(eventHTML);
+			});
+
   });
 })(jQuery);
