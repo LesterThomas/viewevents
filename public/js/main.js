2ab19b7181c328a81d708cf83dd3a7782fc4f56d
Lester <lesterthomas@hotmail.com>
2014-12-25 19:08:38 +0000

    Improved display by grouping in 2 rows

--- public/js/main.js
+++ public/js/main.js
@@ -3,8 +3,8 @@
     // you awesome code goes here
     var oneWeekAgo;
     var oneDayAgo;
-	oneWeekAgo=Date.now()-7*24*60*60*1000;
-	oneDayAgo=Date.now()-1*24*60*60*1000;
+	oneWeekAgo=Date.now()-14*24*60*60*1000;
+	oneDayAgo=Date.now()-2*24*60*60*1000;
 	var peopleObject=[{Name:'Lester'},{Name:'Kate'}];
 	var timelineObjectArray=[];
 	var ObjectId=1;
@@ -13,8 +13,14 @@
     $.getJSON( '/db/_all_docs?startkey="event:' + oneWeekAgo + '"&endkey="event:9417996505591"&include_docs=true' , function( indata ) {
 			  //alert(JSON.stringify(data));
 			  var jsonEvents=indata;
-
 			  var eventsArray=jsonEvents.rows;
+
+			  // create a data set with groups
+			  //var names = ['John', 'Alston', 'Lee', 'Grant'];
+			  var groups = new vis.DataSet();
+			  for (var g = 0; g < peopleObject.length; g++) {
+			    groups.add({id: g, content: peopleObject[g].Name});
+			  }
 			  
 			  for(var i=0;i<eventsArray.length;i++){
 			  	var eventId=eventsArray[i].id;
@@ -40,7 +46,7 @@
 								peopleObject[j].end=theDate;
 								peopleObject[j].id=ObjectId;
 								ObjectId=ObjectId+1;
-								var newObject={id:peopleObject[j].id, content:peopleObject[j].content, start:peopleObject[j].start, end: peopleObject[j].end, group:peopleObject[j].Name };
+								var newObject={id:peopleObject[j].id, content:'', start:peopleObject[j].start, end: peopleObject[j].end, group:j};
 								peopleObject[j].start=null;
 								peopleObject[j].end=null;
 								peopleObject[j].content=null;
@@ -65,20 +71,25 @@
 			      {id: 3, content: 'item 3', start: '2013-04-18'},
 			      {id: 4, content: 'item 4', start: '2013-04-16', end: '2013-04-19'},
 			      {id: 5, content: 'item 5', start: '2013-04-25'},
-			      {id: 6, content: 'item 6', start: '2013-04-27'}
+			      {id: 6, content: 'item 6', start: '2013-04-27'} 
 			    ]);*/
-				var data = new vis.DataSet(timelineObjectArray);
+				var items = new vis.DataSet(timelineObjectArray);
 
 
 
 			    // Configuration for the Timeline
 			    var options = {start:oneDayAgo,end: Date.now(), groupOrder:'content'};
 			    var margin={};
-				margin.item=1;
+				margin.item={};
+				margin.item.horizontal=0;
 				options.margin=margin;
 			    //alert(container.innerHTML);
 			    // Create a Timeline
-			    var timeline = new vis.Timeline(container, data, options);
+
+			    var timeline = new vis.Timeline(container);
+				timeline.setOptions(options);
+				timeline.setGroups(groups);
+				timeline.setItems(items);
 			    //alert(container.innerHTML);
 			});
 
